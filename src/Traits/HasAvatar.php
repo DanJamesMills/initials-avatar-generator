27126a2a7c57bdb724f7ed418058bfa4a42709f2
Danny Mills <daniel620@hotmail.co.uk>
2021-07-11 21:07:56 +0100

    Added trait

---
+++ src/Traits/HasAvatar.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DanJamesMills\InitialsAvatarGenerator\Traits;
+
+trait HasAvatar
+{
+    /* Map default colums */
+    protected static $userIdField = 'user_id';
+
+    protected static function bootHasAvatar()
+    {
+        if (!app()->runningInConsole()) {
+            static::creating(function ($model) {
+                InitialsAvatarGenerator::name('Danny Mills')
+                    ->rounded()
+                    ->generate();
+                
+                // $model->{static::$userIdField} = Auth::User()->id;
+            });
+        }
+    }
+}
